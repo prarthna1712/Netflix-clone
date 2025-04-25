121ffaa66c183ac290d0384527fcf14645f1ee2a
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-04-25 20:12:59 +0530

    Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-
-const App = () => {
+import './Home.css'
+const Home = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Home
