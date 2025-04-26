95bc5eae016c038910af3ab12c90e5dae244079c
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-04-26 10:48:01 +0530

    components

---
+++ src/components/TitleCarts/Titlecarts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Titlecarts.css'
+
+const Titlecarts = () => {
+  return (
+    <div className='titlecarts'>
+      
+    </div>
+  )
+}
+
+export default Titlecarts
