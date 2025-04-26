2051603cc77c86ae49836d370f66b1ce6e92a034
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-04-26 20:40:27 +0530

    titlecard

--- src/components/TitleCarts/Titlecarts.jsx
+++ src/components/TitleCarts/Titlecarts.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Titlecarts.css'
+import cards_data from '../../assets/cards/Cards_data'
+
 
 const Titlecarts = () => {
   return (
-    <div className='titlecarts'>
-      
+    <div className='title-carts'>
+      <h2>Popular on Netflix</h2>
+      <div className="cart-list" ref={cardsRef}>
+        {cards_data.map((card, index)=>{
+          return <div className='cart' key={index}>
+            <img src={card.image} alt=''/>
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,15 +3,24 @@ import './Home.css'
 import Navbar from '../../components/NavBar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import Titlecarts from '../../components/TitleCarts/Titlecarts'
 const Home = () => {
   return (
     <div className='home'>
         <Navbar/>
-        <div className="hero"></div>
+        <div className="hero">
         <img src={hero_banner} alt='' className='banner-img'/>
         <div className="hero-caption">
             <img src={hero_title} alt='' className='caption-img'/>
             <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+        <div className="hero-btn">
+            <button className='btn'><img src={play_icon} alt=''/>Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt=''/>more info</button>
+        </div>
+        <Titlecarts/>
+        </div>
         </div>
     </div>
   )
