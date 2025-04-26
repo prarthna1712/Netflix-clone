3bcff2abfb35d75183e96848240b6f2a001549b5
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-04-26 17:27:49 +0530

    navbar css

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search from '../../assets/search_icon.svg'
+import bell from '../../assets/bell_icon.svg'
+import profile from '../../assets/profile_img.png'
+import caret from '../../assets/caret_icon.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className="navbar-left">
+        <img src={logo} alt='' />
+        <ul>
+          <li>Home</li>
+          <li>TV Show</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Language</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={search} alt='' className='icon'/>
+        <p>Children</p>
+        <img src={bell} alt='' className='icon'/>
+      <div className="navbar-profile">
+        <img src={profile} alt='' className='profile'/>
+        <img src={caret} alt='' />
+      </div>
+      </div>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Home.css'
+import Navbar from '../../components/NavBar/Navbar'
 const Home = () => {
   return (
     <div className='home'>
+        <Navbar/>
       
     </div>
   )
