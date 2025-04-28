3906f7394d1ddb0b50ab6b55a48f21e4008ad473
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-04-28 16:26:26 +0530

    Login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 
 const Login = () => {
+
+  const[signSate, setSignSet] = useState("Sign In")
+
   return (
     <div className='login'>
       <img src={logo} className='login-logo' alt=''/>
       <div className="login-form">
-        <h1>Sign Up</h1>
+        <h1>{signSate}</h1>
         <form>
-          <input type='text' placeholder='Your name'/>
+          {signSate==="Sign Up"?<input type='text' placeholder='Your name'/>:<></>}
           <input type='email' placeholder='Email'/>
           <input type='password' placeholder='Password'/>
-          <button>Sign Up</button>
+          <button>{signSate}</button>
           <div className="form-help">
             <div className="remember">
               <input type='checkbox'/>
@@ -21,8 +24,14 @@ const Login = () => {
             <p>Need Help?</p>
           </div>
         </form>
+        <div className="form-switch">
+          {signSate==="Sign In"?<p>New to Netflix? 
+            <span>Sign Up Now</span></p>: 
+            <p>Already have Account? <span>Sign In Now</span></p>}
+          
+         
+        </div>
       </div>
-      
     </div>
   )
 }
