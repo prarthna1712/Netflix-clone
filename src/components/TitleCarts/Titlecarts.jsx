66786d917937ac954bee3d97d7aa6f5835b29cf2
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-04-29 18:57:01 +0530

    Youtube trailer

--- src/components/TitleCarts/Titlecarts.jsx
+++ src/components/TitleCarts/Titlecarts.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Titlecarts.css'
 import cards_data from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom';
 
 
 const Titlecarts = ({title,category}) => {
@@ -28,10 +29,10 @@ const Titlecarts = ({title,category}) => {
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="cart-list">
         {apiData.map((card, index)=>{
-          return <div className='cart' key={index}>
+          return <Link to={`/player/${card.id}`} className='cart' key={index}>
             <img src= {`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt=''/>
             <p>{card.original_title}</p>
-          </div>
+          </Link>
         })}
       </div>
     </div>
--- src/pages/Player/player.jsx
+++ src/pages/Player/player.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const{id} = useParams();
+
+  const [apiData,setApiData] = useState({
+    name:"",
+    key:"",
+    published_at:"",
+    typeof:""
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NmNjZjYyYTU5M2ZlMTEzNDQzNzhhYjNhZTE4NDc0MyIsIm5iZiI6MTc0NTgzODk0My44MjA5OTk5LCJzdWIiOiI2ODBmNjM1ZjcxZGVkY2I4YWNlYjM5ZjQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.xMb6jsDb78INP7pyckzeSYmM3e08tfnNPFuQxmxYjjg'
+    }
+  };
+  
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results[0]))
+    .catch(err => console.error(err));
+
+  },[])
+  
   return (
     <div className='player'>
       <img src={back_arrow_icon} alt=''/>
-      <iframe width='90%' height='90%' src={'https://www.youtube.com/embed/Yvh96Vb8_AE&t=181s'}
+      <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`}
       title='trailer' frameBorder='0' allowFullScreen></iframe>
       <div className="player-info">
-        <p>Published Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
       </div>
     </div>
   )
